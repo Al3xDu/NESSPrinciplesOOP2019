8858b2566f4382207290849de4876bd2f26932ee
Eduard Popescu <eduard.popescu@ness.com>
2019-09-21 11:34:08 +0300

    DIP

---
+++ DIP/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace DIP
+{
+	using System;
+	using System.Linq;
+	using HighLevel;
+
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			var a = new A();
+
+			a.DoHighLevelStuff();
+		}
+	}
+}
+
+namespace HighLevel
+{
+	using System;
+	using System.Linq;
+	using LowLevel;
+
+	public class A
+	{
+		private readonly B b;
+
+		public A() => b = new B();
+
+		public void DoHighLevelStuff()
+		{
+			b.DoLowLevelStuff();
+		}
+	}
+}
+
+namespace LowLevel
+{
+	using System;
+	using System.Linq;
+
+	public class B
+	{
+		public void DoLowLevelStuff() { }
+	}
+}
